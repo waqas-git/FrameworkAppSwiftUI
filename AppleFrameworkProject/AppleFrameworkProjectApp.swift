2d7a722cd0c1264cbb036c344c94ad9b73115654
Waqas Ahmed <waqasahmed.uol@gmail.com>
2024-08-08 13:46:26 +0500

    Initial Commit

---
+++ AppleFrameworkProject/AppleFrameworkProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworkProjectApp.swift
+//  AppleFrameworkProject
+//
+//  Created by waqas ahmed on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworkProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
